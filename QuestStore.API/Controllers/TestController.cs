39049f5000769dafd3c38879c02473654a15cd31
Paweł Bednarczyk <bedpaw97@gmail.com>
2020-09-22 18:05:14 +0200

    #1 Add basic controller to test swagger config

---
+++ QuestStore.API/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuestStore.API.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetCustomers()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
