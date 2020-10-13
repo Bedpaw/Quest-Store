3f5fc3e7e11c04812654a317a205e8fdc772a4de
Paweł Bednarczyk <bedpaw97@gmail.com>
2020-10-13 11:40:29 +0200

    Add test page

---
+++ Web/SPA Vue/src/views/Profile.vue
@@ -0,0 +1,34 @@
+<template>
+<v-card>
+  <v-container>
+    <v-row class="d-flex justify-start">
+      <v-col>
+        <v-avatar>
+          <img :src="$auth.user.picture">
+        </v-avatar>
+      </v-col>
+      <v-col>
+        <v-card-title>{{$auth.user.name}}</v-card-title>
+      </v-col>
+    </v-row>
+  </v-container>
+  <div>
+
+
+  </div>
+</v-card>
+</template>
+
+<script>
+
+export default {
+name: "Profile",
+created() {
+  console.log(this.$auth.user)
+}
+}
+</script>
+
+<style scoped>
+
+</style>
