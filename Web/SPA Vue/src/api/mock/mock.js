12d2b5e29f7ea3e68a06278ef83f80a3f322d337
Paweł Bednarczyk <bedpaw97@gmail.com>
2020-10-13 11:39:10 +0200

    Reorganize auth test function

--- Web/SPA Vue/src/api/mock/mock.js
+++ Web/SPA Vue/src/api/mock/mock.js
@@ -1,9 +1,29 @@
+import axios from "axios";
+import {userController} from "@/api/mock/user-controller";
 
 
 
+async function testApi() {
+  // Get the access token from the auth wrapper
+  console.log(this.$auth)
+  const token = await this.$auth.getTokenSilently();
+  console.log(token)
+
+  // Use Axios to make a call to the API
+  const {data} = await axios.get(
+    "https://localhost:5001/api/test/welcomeadmin",
+    {
+      headers: {
+        Authorization: `Bearer ${token}` // send the access token through the 'Authorization' header
+      }
+    }
+  );
+  console.log(data);
+  return data;
+}
 
 
 export const mockApi = {
-  ...eventController,
   ...userController,
+  testApi
 };
--- Web/SPA Vue/src/api/mock/user-controller.js
+++ Web/SPA Vue/src/api/mock/user-controller.js
@@ -1,6 +1,5 @@
 export const userController = {
-
   getUsers() {
-    return
+    return ''
   }
 }
