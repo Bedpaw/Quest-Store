d8fd9422df6955c11672fd41b15a7631216a5c96
zdulak <dpzdulski@gmail.com>
2020-10-16 21:17:10 +0200

    Repository methods made virtual and its members made protected.

--- QuestStore.Infrastructure.Data/Repository/Repository.cs
+++ QuestStore.Infrastructure.Data/Repository/Repository.cs
@@ -10,36 +10,36 @@ namespace QuestStore.Infrastructure.Data.Repository
 {
     public class Repository<T> : IRepository<T> where T : BaseEntity, new()
     {
-        private readonly StoreDbContext _context;
-        private readonly DbSet<T> _entities;
+        protected readonly StoreDbContext Context; 
+        protected readonly DbSet<T> Entities;
 
         public Repository(StoreDbContext context)
         {
-            _context = context;
-            _entities = context.Set<T>();
+            Context = context;
+            Entities = context.Set<T>();
         }
 
-        public async Task<IEnumerable<T>> GetAll() => await _entities.ToListAsync();
-        public async Task<T> GetById(int id) => await _entities.FindAsync(id);
+        public virtual async Task<IEnumerable<T>> GetAll() => await Entities.ToListAsync();
+        public virtual async Task<T> GetById(int id) => await Entities.FindAsync(id);
 
-        public async Task Add(T entity)
+        public virtual async Task Add(T entity)
         {
             if (entity == null) throw new ArgumentNullException(nameof(entity));
-            _entities.Add(entity);
-            await _context.SaveChangesAsync();
+            Entities.Add(entity);
+            await Context.SaveChangesAsync();
         }
 
-        public async Task Update(T entity)
+        public virtual async Task Update(T entity)
         {
             if (entity == null) throw new ArgumentNullException(nameof(entity));
-            _entities.Update(entity);
-            await _context.SaveChangesAsync();
+            Entities.Update(entity);
+            await Context.SaveChangesAsync();
         }
 
-        public async Task DeleteById(int id)
+        public virtual async Task DeleteById(int id)
         {
-            _entities.Remove(new T {Id = id});
-            await _context.SaveChangesAsync();
+            Entities.Remove(new T {Id = id});
+            await Context.SaveChangesAsync();
         }
     }
 }
